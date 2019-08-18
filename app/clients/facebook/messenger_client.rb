f62bb2d52933a5a954f64185e0a589bf7a37954d
Evgeniy <alabushev89@gmail.com>
2019-08-18 16:45:24 +0500

    fix bags

--- app/clients/facebook/messenger_client.rb
+++ app/clients/facebook/messenger_client.rb
@@ -1,41 +1,35 @@
-module Facebook
-  class MessengerClient
-    def send_message(message)
-      begin
-        response = faraday_connection.post("me/messages?access_token=#{page_access_token}", message)
-        response.body if response&.success?
-      rescue StandardError
-        nil
-      end
-    end
+class MessengerClient
+  def send_message(message)
+    response = faraday_connection.post("me/messages?access_token=#{page_access_token}", message)
+    response.body if response&.success?
+  end
 
-    def user_info(sender_id)
-      begin
-        response = faraday_connection.get("#{sender_id}?access_token=#{page_access_token}")
-        response.body if response&.success?
-      rescue StandardError
-        nil
-      end
+  def user_info(sender_id)
+    begin
+      response = faraday_connection.get("#{sender_id}?access_token=#{page_access_token}")
+      response.body if response&.success?
+    rescue StandardError
+      nil
     end
+  end
 
-    private
+  private
 
-    def faraday_connection
-      connection = Faraday.new(url: graph_url) do |faraday|
-        faraday.response :json
-        faraday.request :json
-        faraday.request :url_encoded
-        faraday.adapter Faraday.default_adapter
-      end
-      connection
+  def faraday_connection
+    connection = Faraday.new(url: graph_url) do |faraday|
+      faraday.response :json
+      faraday.request :json
+      faraday.request :url_encoded
+      faraday.adapter Faraday.default_adapter
     end
+    connection
+  end
 
-    def page_access_token
-      ENV['FACEBOOK_PAGE_ACCESS_TOKEN']
-    end
+  def page_access_token
+    ENV['FACEBOOK_PAGE_ACCESS_TOKEN']
+  end
 
-    def graph_url
-      ENV['FACEBOOK_GRAPH_BASE_URL']
-    end
+  def graph_url
+    ENV['FACEBOOK_GRAPH_BASE_URL']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :webhooks do
-    namespace :facebook do
-      resources :bot, only: [:index, :create]
-    end
+  namespace :facebook do
+    resources :webhook, only: [:index, :create]
   end
 end
