fccdb3a00933826e000113382ff1c55bb5208724
Evgeniy <alabushev89@gmail.com>
2019-08-23 00:23:40 +0500

    add action create to webhook resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :facebook do
-    resources :webhooks, only: [:index]
+    resources :webhooks, only: [:index, :post]
     resources :messages, only: [:create]
   end
 end
