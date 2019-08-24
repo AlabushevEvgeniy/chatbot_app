77736a13d77848163f4834e3e80913f530df8abc
Evgeniy <alabushev89@gmail.com>
2019-08-24 20:55:50 +0500

    message_controller change message to recipient anf message

--- app/controllers/facebook/messages_controller.rb
+++ app/controllers/facebook/messages_controller.rb
@@ -1,7 +1,15 @@
 class Facebook::MessagesController < ApplicationController
 
   def create
-    Facebook::MessengerClient.new.send_message('hello world!!!')
+    Facebook::MessengerClient.new.send_message(
+                      "recipient": {
+                        "id":"112032206819748"
+                      },
+                      "message": {
+                        "text": "hello, world!"
+                      }
+                    }
+                  )
     head :ok
   end
 end
